38f414ea9266f880bff397db572dd94d2bcbd250
Connor Luebbehusen <connorluebbehusen@gmail.com>
2020-10-25 14:30:18 -0500

    Switched use of text files to a single json config file

--- eInkPyClock.py
+++ eInkPyClock.py
@@ -1,10 +1,18 @@
+import json
+import sys
+
 from src.requests.bitcoin import get_bitcoin_price
 from src.requests.weather import get_weather
 from src.requests.spotify import get_spotify
 
-import json
 
 if __name__ == '__main__':
+    try:
+        with open('config.json', 'r') as config_file:
+            config = json.load(config_file)
+    except FileNotFoundError:
+        print('Failed to open config file')
+        sys.exit(1)
     print(get_bitcoin_price())
-    print(json.dumps(get_weather()))
-    print(get_spotify())
+    print(json.dumps(get_weather(config['weather'])))
+    print(get_spotify(config['spotify']))
