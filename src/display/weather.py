b9e61f8d76c205e07f87052530edb6c9d6f0450a
Connor Luebbehusen <connorluebbehusen@gmail.com>
2020-10-25 19:33:19 -0500

    Third image writing test

--- src/display/weather.py
+++ src/display/weather.py
@@ -12,7 +12,7 @@ partly_cloudy_icon = Image.open('icons/Partly_Cloudy.png')
 rain_icon = Image.open('icons/Rain.png')
 snow_icon = Image.open('icons/Snow.png')
 sun_icon = Image.open('icons/Sun.png')
-thunderstorm_icon = Image('icons/Thunderstorm.png')
+thunderstorm_icon = Image.open('icons/Thunderstorm.png')
 
 
 def draw_weather_icon():
@@ -24,5 +24,12 @@ def draw_weather_icon():
     image.paste(album_icon, (25, 75))
     image.paste(artist_icon, (25, 125))
     image.paste(playlist_icon, (25, 175))
+    image.paste(cloudy_icon, (25, 225))
+    image.paste(mist_icon, (75, 25))
+    image.paste(partly_cloudy_icon, (75, 75))
+    image.paste(rain_icon, (75, 125))
+    image.paste(snow_icon, (75, 175))
+    image.paste(sun_icon, (75, 225))
+    image.paste(thunderstorm_icon, (125, 25))
     image_buffer = epd.getbuffer(image)
     epd.display(image_buffer)
