05284c682ef163b509ffbb41075e40172710d886
Connor Luebbehusen <connorluebbehusen@gmail.com>
2020-10-27 06:36:19 -0500

    Testing new bitcoin icons

--- src/display/bitcoin.py
+++ src/display/bitcoin.py
@@ -9,7 +9,7 @@ icon_height = 48
 def add_bitcoin_graphics(image, draw, xoffset, yoffset, bitcoin_price):
     add_bitcoin_icon(image, xoffset, yoffset)
     btc = '$' + '{0:,.2f}'.format(bitcoin_price)
-    draw.text((xoffset + icon_width + 20, 0), btc, font=fk64)
+    draw.text((xoffset + icon_width + 140, 0), btc, font=fk64)
 
 
 def add_bitcoin_icon(image, xoffset, yoffset):
