aa8d7ad034d00115fac7df496b9bd4f6f39eb42a
Connor Luebbehusen <connorluebbehusen@gmail.com>
2020-10-27 13:29:23 -0500

    More adjustments to bitcoin display

--- src/display/bitcoin.py
+++ src/display/bitcoin.py
@@ -6,10 +6,10 @@ eink_width = 400
 
 
 def add_bitcoin_graphics(image, draw, xoffset, yoffset, bitcoin_price):
-    one = '1 '
+    one = '1'
     draw.text((xoffset, yoffset / 2), one, font=fk64)
-    add_bitcoin_icon(image, xoffset + find_string_width(one, 2), yoffset)
-    bitcoin_string = '= $' + '{0:,.2f}'.format(bitcoin_price)
+    add_bitcoin_icon(image, xoffset + find_string_width(one, 2) * 2, yoffset)
+    bitcoin_string = '$' + '{0:,.2f}'.format(bitcoin_price)
     price_xoffset = (eink_width - find_string_width(bitcoin_string, 2) -
                      xoffset)
     draw.text((price_xoffset, yoffset / 2), bitcoin_string, font=fk64)
