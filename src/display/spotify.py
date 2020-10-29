8de8b8e279261399146ddcaf49c58ab810cbfe98
Connor Luebbehusen <connorluebbehusen@gmail.com>
2020-10-29 16:59:36 -0500

    First pass at adding name and type and modifed text formatting

--- src/display/spotify.py
+++ src/display/spotify.py
@@ -1,50 +1,42 @@
 from PIL import Image
-from src.util.fonts import ds32, ds16, find_string_width
+from src.util.fonts import ds32, find_string_width
 
+playlist_icon = Image.open('icons/Playlist.png')
+artist_icon = Image.open('icons/Artist.png')
+album_icon = Image.open('icons/Album.png')
 eink_width = 400
 
 
 def format_text(width, text):
+    text = text.split('(')[0]
+    text = text.split(',')[0]
     if (find_string_width(text, 1) <= width):
-        return [text]
-    split_text = text.split()
-    return_list = ['', '']
-    split_index = 0
-    for word in split_text:
-        if (find_string_width(return_list[0] + word + ' ', 1) <= width):
-            return_list[0] += (word + ' ')
-            split_index += 1
-    return_list[0] = return_list[0][0:-1]
-    for word in split_text[split_index:]:
-        if (len(split_text) == split_index + 1):
-            if (find_string_width(return_list[1] + word, 1) <= width):
-                return_list[1] += word + ' '
-                split_index += 1
-        elif (find_string_width(return_list[1] + word + '...', 1) <= width):
-            return_list[1] += word + ' '
-            split_index += 1
-        else:
-            break
-    return_list[1] = return_list[1][0:-1]
-    if len(split_text) > split_index:
-        return_list[1] += '...'
-    return return_list
+        return text
+    shortened_text = ''
+    for char in text:
+        if (find_string_width(shortened_text + char) <= width):
+            shortened_text += char
+    shortened_text = shortened_text[0:-3] + '...'
+    return shortened_text
 
 
 def add_spotify_graphics(image, draw, xoffset, yoffset, spotify_info):
     formatted_track = format_text(eink_width - xoffset - 8,
                                   spotify_info['track'])
-    add_track_name(draw, xoffset, yoffset, formatted_track)
-    yaddition = 36
-    if len(formatted_track) > 1:
-        yaddition = 72
-    add_artist_name(draw, xoffset, yoffset + yaddition, spotify_info['artist'])
-
-
-def add_track_name(draw, xoffset, yoffset, formatted_track):
-    for line in formatted_track:
-        draw.text((xoffset, yoffset), line, font=ds32)
-
-
-def add_artist_name(draw, xoffset, yoffset, artist):
-    draw.text((xoffset, yoffset), artist, font=ds32)
+    formatted_artist = format_text(eink_width - xoffset - 8,
+                                   spotify_info['artist'])
+    image_offset = xoffset
+    type = spotify_info['type']
+    if type:
+        image_offset += 32
+        icon = album_icon
+        if type == 'playlist':
+            icon = playlist_icon
+        if type == 'artist':
+            icon = artist_icon
+        image.paste(icon, (xoffset, yoffset + 72))
+    formatted_name = format_text(eink_width - image_offset - 8,
+                                 spotify_info['name'])
+    draw.text((xoffset, yoffset), formatted_track, font=ds32)
+    draw.text((xoffset, yoffset + 32), formatted_artist, font=ds32)
+    draw.text((image_offset, yoffset + 72), formatted_name, font=ds32)
